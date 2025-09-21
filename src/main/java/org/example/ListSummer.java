f1d6e693802aed1eceb6a1fb3d11625565f93958
kenneth <kenneth12345645@gmail.com>
2025-09-21 13:50:36 +0800

    Complete ListSummer multi threading

--- src/main/java/org/example/ListSummer.java
+++ src/main/java/org/example/ListSummer.java
@@ -1,7 +1,30 @@
 package org.example;
 
-public class ListSummer {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class ListSummer implements Runnable{
+
+    private List<Integer> lst;
+
+    public ListSummer(List<Integer> lst) {
+        this.lst = lst;
+    }
+
+    public void addNum(int num) {
+        lst.add(num);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            int sum = lst.stream().reduce(0 , Integer::sum);
+            System.out.println(sum);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.err.println("Terminating Summer");
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> lst = new ArrayList<>();
+        lst.add(1);
+        lst.add(2);
+        ListSummer task = new ListSummer(lst);
+        Thread t1 = new Thread(task);
+        t1.start();
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter a number to add to the list");
+            int num = Integer.parseInt(scanner.nextLine());
+            task.addNum(num);
+        }
     }
 }
