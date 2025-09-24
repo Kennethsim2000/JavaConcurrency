9148adb07fdda62f30a2d32a0a9643540f6389e7
kenneth <kenneth12345645@gmail.com>
2025-09-24 19:04:35 +0800

    Add Cyclic barrier to synchronise threads

--- src/main/java/org/example/SynchExecution.java
+++ src/main/java/org/example/SynchExecution.java
@@ -1,24 +1,29 @@
 package org.example;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class SynchExecution implements Runnable {
 
     int sleepTime;
     CountDownLatch latch;
+    CyclicBarrier barrier;
 
-    public SynchExecution(int sleepTime, CountDownLatch latch) {
+    public SynchExecution(int sleepTime, CountDownLatch latch, CyclicBarrier barrier) {
         this.sleepTime = sleepTime;
         this.latch = latch;
+        this.barrier = barrier;
     }
 
     @Override
     public void run() {
         try {
             Thread.sleep(sleepTime * 1000);
+            barrier.await();
             System.out.println("Thread has finished sleeping");
             latch.countDown();
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | BrokenBarrierException e) {
             throw new RuntimeException(e);
         }
 
--- src/main/java/org/example/SynchExecutionMain.java
+++ src/main/java/org/example/SynchExecutionMain.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class SynchExecutionMain {
 
     public static void main(String[] args) {
         CountDownLatch latch = new CountDownLatch(5);
+        CyclicBarrier barrier = new CyclicBarrier(5, ()-> System.out.println("All threads have reached barrier"));
+
         for(int i = 0; i < 5; i++) {
-            Thread t = new Thread(new SynchExecution(5 - i, latch));
+            Thread t = new Thread(new SynchExecution(5 - i, latch, barrier));
             t.start();
         }
         try {
